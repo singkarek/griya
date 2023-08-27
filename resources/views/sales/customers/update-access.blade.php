c7bcc0aa80232aa0bce847b9ae49474daca7e73b
Samsut Tabris <samsuttabris.st@gmail.com>
2023-08-27 17:51:44 +0700

    push map

--- resources/views/sales/customers/update-access.blade.php
+++ resources/views/sales/customers/update-access.blade.php
@@ -18,7 +18,7 @@
 @endsection
 
 <script
-    src="https://maps.googleapis.com/maps/api/js?key={{ env('GMAP_KEY') }}&callback=initMap&v=weekly"
+    src="https://maps.googleapis.com/maps/api/js?key={{ confiig('app.gmap_key') }}&callback=initMap&v=weekly"
     defer
     >
 </script>
